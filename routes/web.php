d5f666e5649a83bfaf7529ab2d49685ee982520a
Bahaa Moqayad <bahaa200030.m7taref@gmail.com>
2022-06-25 21:28:41 +0300

    Commit 2

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MealController;
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/',[SiteController::class,'index'])->name('site.index');
+
+Route::prefix('/admin')->name('admin.')->middleware(['auth','is_admin'])->group(function(){
+    // Route::get('/',[AdminController::class,'index'])->name('index');
+    // Route::resource('/categories',[CategoryController::class,'index'])->name('category');
+    // Route::resource('/meals',[MealController::class,'index'])->name('category');
 });
